418e21a9485fe93c03a4ccbb657f5c8cd9230a0e
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-04 06:50:52 +0700

    Organize the folder structure according to the layered architecture

---
+++ src/main/java/com/java/springrestful/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.java.springrestful.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
---
+++ src/main/java/com/java/springrestful/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.java.springrestful.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+
+}
