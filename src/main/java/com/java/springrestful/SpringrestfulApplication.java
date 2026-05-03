9031d8b2e003beb32d838265e41c8f5b6b5d9a11
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-04 05:21:36 +0700

    Initialize a Java Spring project with Gradle-Kotlin

---
+++ src/main/java/com/java/springrestful/SpringrestfulApplication.java
@@ -0,0 +1,18 @@
+package com.java.springrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+//disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+public class SpringrestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/springrestful/SpringrestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
