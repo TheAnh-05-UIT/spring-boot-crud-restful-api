67bdd6a2ab982280f58b76de1dab3e41b91698ea
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-04 08:07:38 +0700

    feat(user): implement delete user by id API

--- src/main/java/com/java/springrestful/controller/UserController.java
+++ src/main/java/com/java/springrestful/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +39,11 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User newUser = this.userService.handleCreateUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @DeleteMapping("users/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable("id") Long id) {
+        this.userService.handleDeleteUserById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Delete Success");
     }
 }
--- src/main/java/com/java/springrestful/service/UserService.java
+++ src/main/java/com/java/springrestful/service/UserService.java
@@ -29,4 +29,8 @@ public class UserService {
     public User handleCreateUser(User user) {
         return this.userRepository.save(user);
     }
+
+    public void handleDeleteUserById(Long id) {
+        this.userRepository.deleteById(id);
+    }
 }
