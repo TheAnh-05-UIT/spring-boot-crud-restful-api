7f47cb07efb1e7ba51896c8aa7ebcfc1aeb31520
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-05 22:36:58 +0700

    feat(dto): add LoginDTO with validation for login

---
+++ src/main/java/com/java/springrestful/domain/dto/LoginDTO.java
@@ -0,0 +1,29 @@
+package com.java.springrestful.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginDTO {
+
+    @NotBlank(message = "Email cannot be empty")
+    private String username;
+
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
