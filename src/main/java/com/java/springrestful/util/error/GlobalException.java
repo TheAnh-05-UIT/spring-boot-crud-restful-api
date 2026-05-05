8b4311903086292359c80b53cb0d4d11dd2edb85
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-05 23:19:41 +0700

    feat(exception): handle validation errors with GlobalException

---
+++ src/main/java/com/java/springrestful/util/error/GlobalException.java
@@ -0,0 +1,34 @@
+package com.java.springrestful.util.error;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.java.springrestful.domain.RestResponse;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> validationError(MethodArgumentNotValidException validException) {
+        BindingResult result = validException.getBindingResult();
+        final List<FieldError> fieldErrors = result.getFieldErrors();
+
+        RestResponse<Object> res = new RestResponse<>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(validException.getBody().getDetail());
+
+        List<String> errors = fieldErrors.stream().map(field -> field.getDefaultMessage()).collect(Collectors.toList());
+
+        res.setMessage(errors.size() > 1 ? errors : errors.get(0));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+}
