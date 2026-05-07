337adda8ce1bc02dc52deef75098bf95be9eec6d
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-08 04:21:32 +0700

    feat: implement JWT-based authentication with custom entry point and security configuration

--- src/main/java/com/java/springrestful/controller/AuthController.java
+++ src/main/java/com/java/springrestful/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,8 @@ public class AuthController {
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
         String newToken = this.securityService.createToken(authentication);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         return ResponseEntity.ok().body(newToken);
     }
 }
