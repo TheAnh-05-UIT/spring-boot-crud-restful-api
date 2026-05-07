9bc7e80875376f9328e5274043207a7cc23960ab
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-08 04:21:37 +0700

    feat: implement JWT-based security configuration with custom authentication error handling

---
+++ src/main/java/com/java/springrestful/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,45 @@
+package com.java.springrestful.config;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.java.springrestful.domain.RestResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final AuthenticationEntryPoint authenticationEntryPoint = new BearerTokenAuthenticationEntryPoint();
+
+    private ObjectMapper objectMapper;
+
+    public CustomAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+
+        this.authenticationEntryPoint.commence(request, response, authException);
+        response.setContentType("application/json;charset=UTF-8");
+
+        RestResponse<Object> restResponse = new RestResponse<>();
+        restResponse.setStatusCode(HttpStatus.UNAUTHORIZED.value());
+        restResponse.setError(authException.getCause().getMessage());
+        restResponse.setMessage("Token không hợp lệ hoặc đã hết hạn (expired)...");
+        objectMapper.writeValue(response.getWriter(), restResponse);
+    }
+
+}
