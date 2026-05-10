1e13077a05f4d1423a66c11a394f0e000ed00149
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-10 17:39:40 +0700

    feat(company): Create a new endpoint to retrieve all companies

--- src/main/java/com/java/springrestful/controller/CompanyController.java
+++ src/main/java/com/java/springrestful/controller/CompanyController.java
@@ -1,8 +1,14 @@
 package com.java.springrestful.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +30,12 @@ public class CompanyController {
         Company newCompany = this.companyService.handleCreateCompany(company);
         return ResponseEntity.status(HttpStatus.CREATED).body(newCompany);
     }
+
+    @GetMapping("/companies")
+    public ResponseEntity<List<Company>> getAllCompany() {
+
+        List<Company> listCompany = this.companyService.handleGetAllCompany();
+        return ResponseEntity.status(HttpStatus.OK).body(listCompany);
+    }
+
 }
--- src/main/java/com/java/springrestful/service/CompanyService.java
+++ src/main/java/com/java/springrestful/service/CompanyService.java
@@ -1,5 +1,8 @@
 package com.java.springrestful.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.java.springrestful.domain.Company;
@@ -17,4 +20,9 @@ public class CompanyService {
     public Company handleCreateCompany(Company company) {
         return this.companyRepository.save(company);
     }
+
+    public List<Company> handleGetAllCompany() {
+        return this.companyRepository.findAll();
+    }
+
 }
