ac89eccb4ad9661e2ddbdcc61b31d8f692df3822
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-14 12:08:13 +0700

    feat(user): Update User model

--- src/main/java/com/java/springrestful/domain/User.java
+++ src/main/java/com/java/springrestful/domain/User.java
@@ -1,6 +1,12 @@
 package com.java.springrestful.domain;
 
+import java.time.Instant;
+
+import com.java.springrestful.util.constant.GenderEnum;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,6 +23,82 @@ public class User {
     private String name;
     private String email;
     private String password;
+
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
+
+    private String address;
+    private String refreshToken;
+    private Instant createAt;
+    private Instant updateAt;
+    private String createBy;
+    private String updateBy;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public GenderEnum getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderEnum gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public Instant getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Instant createAt) {
+        this.createAt = createAt;
+    }
+
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Instant updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
 
     public Long getId() {
         return id;
---
+++ src/main/java/com/java/springrestful/util/constant/GenderEnum.java
@@ -0,0 +1,7 @@
+package com.java.springrestful.util.constant;
+
+public enum GenderEnum {
+    FEMALE,
+    MALE,
+    ORTHER
+}
